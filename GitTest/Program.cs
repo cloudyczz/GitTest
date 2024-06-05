8e3846d94e88e6ea310fdce51775161844d45d73
andhi <andhi@ANDHINIAJ>
2024-06-05 10:07:36 +0700

    Add project files.

---
+++ GitTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nama    : ");
+            String nama = Console.ReadLine();
+            Console.Write("Jurusan : ");
+            String jurusan = Console.ReadLine();
+        }
+    }
+}
